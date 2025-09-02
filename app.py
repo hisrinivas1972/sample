ce05516979f22b46a103c1f3493d71f2c9d2967a
hisrinivas1972 <104999742+hisrinivas1972@users.noreply.github.com>
2025-09-02 11:08:45 +0530

    Update app.py

--- app.py
+++ app.py
@@ -247,7 +247,7 @@ if uploaded_employees and uploaded_branches and uploaded_transactions:
         col5, col6, col7, col8 = st.columns(4)
         col5.metric("Total Branches", f"{total_branches}")
         col6.metric("Performance Ratio", f"{performance_ratio:.2f}x")
-        col7.metric((f"**Performance Status:** {perf_status_display}", unsafe_allow_html=True)
+        col7.markdown(f"**Performance Status:** {perf_status_display}", unsafe_allow_html=True)
         col8.metric("Total Employees", total_employees)
 
 
